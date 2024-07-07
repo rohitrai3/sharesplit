28a0d4dc2943e1a613fded4d6fdd4240d3359484
Rohit Rai <rohitraijob@gmail.com>
2024-07-07 05:37:44 +0100

    Landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "gold-light": "#F5DF4D",
+        "gold-dark": "#F0C05A",
+      }
     },
   },
   plugins: [],
