957f9f2077adb899aa7854eddf77a9589959404a
Rohit Rai <rohitraijob@gmail.com>
2024-08-25 17:34:17 +0100

    Add create group API

---
+++ app/api/group/create/route.ts
@@ -0,0 +1,38 @@
+import { Member, Prisma, PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { getSession } from "@auth0/nextjs-auth0";
+
+const prisma = new PrismaClient();
+
+export async function POST(request: NextRequest) {
+  const formData = await request.formData();
+  const { user } = await getSession();
+  const owner: Member | null = await prisma.member.findUnique({
+    where: {
+      name: user.name,
+    },
+  });
+  const name: string = formData.get("name")?.toString()!;
+  const membersName: string[] = formData.get("members")?.toString().split(",")!;
+  membersName.forEach((name, index) => (membersName[index] = name.trim()));
+  const members: Member[] = [owner!];
+  for (const name of membersName) {
+    const member: Member | null = await prisma.member.findUnique({
+      where: {
+        name: name,
+      },
+    });
+    if (member) members.push(member);
+  }
+  const groupInput: Prisma.GroupCreateInput = {
+    name: name,
+    members: {
+      connect: members,
+    },
+  };
+  await prisma.group.create({
+    data: groupInput,
+  });
+
+  return NextResponse.redirect(new URL("/home", request.url));
+}
