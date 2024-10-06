9335e2cb0c8250307e1d2e75abd6283f71e0708f
Rohit Rai <rohitraijob@gmail.com>
2024-10-06 11:55:44 +0530

    Load group info in create expense page

---
+++ app/expense/create/loadGroup.tsx
@@ -0,0 +1,31 @@
+import Group from "@/app/components/group";
+import Loading from "@/app/components/loading";
+import { GroupItem } from "@/app/types";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export function LoadGroup() {
+  const searchParams = useSearchParams();
+  const [group, setGroup] = useState<GroupItem>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    fetch(`/api/group/get/${searchParams.get("groupId")}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setGroup(data);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log("Error fetching group.", err));
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <Group name={group?.name!} members={group?.members!} />
+      )}
+    </>
+  );
+}
--- app/expense/create/page.tsx
+++ app/expense/create/page.tsx
@@ -1,14 +1,18 @@
+"use client";
+
 import Logout from "@/app/components/buttons/logout";
-import Group from "@/app/components/group";
 import User from "@/app/components/user";
 import Form from "./form";
 import { Suspense } from "react";
+import { LoadGroup } from "./loadGroup";
 
 export default function CreateExpense() {
   return (
     <div className="w-dvw h-dvh flex flex-col p-6 space-y-10">
       <User />
-      <Group name="Ino-Shika-Cho" members={[]} />
+      <Suspense>
+        <LoadGroup />
+      </Suspense>
       <div className="flex-1 flex justify-center items-end">
         <Suspense>
           <Form />
