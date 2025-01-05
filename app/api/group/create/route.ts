17e4c8dabfc0b448e212baab35235279928a9f07
Rohit Rai <rohitraijob@gmail.com>
2025-01-05 16:17:03 +0530

    chore: add creator in membersName

--- app/api/group/create/route.ts
+++ app/api/group/create/route.ts
@@ -7,16 +7,14 @@ const prisma = new PrismaClient();
 export async function POST(request: NextRequest) {
   const formData = await request.formData();
   const session = await getSession();
-  const user = session?.user;
-  const owner: Member | null = await prisma.member.findUnique({
-    where: {
-      name: user?.name,
-    },
-  });
+  const creator: string = session?.user?.name;
   const name: string = formData.get("name")?.toString()!;
   const membersName: string[] = formData.get("members")?.toString().split(",")!;
+  const members: Member[] = [];
+
   membersName.forEach((name, index) => (membersName[index] = name.trim()));
-  const members: Member[] = [owner!];
+  membersName.push(creator);
+
   for (const name of membersName) {
     const member: Member | null = await prisma.member.findUnique({
       where: {
