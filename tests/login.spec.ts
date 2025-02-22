bf9848d1b08321b49bb4067046c2eceb4926c793
Rohit Rai <rohitraijob@gmail.com>
2025-02-23 01:50:04 +0530

    test: login page has logo text

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -11,3 +11,9 @@ test("has welcome message", async ({ page }) => {
 
   await expect(page.getByRole("heading", { name: "Welcome to"})).toBeVisible();
 })
+
+test("has logo text", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+
+  await expect(page.getByAltText("ShareSplit text")).toBeVisible();
+})
