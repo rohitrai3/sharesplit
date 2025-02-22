8b7a1b5b3400f0e809bb1e43d690498117c069ff
Rohit Rai <rohitraijob@gmail.com>
2025-02-23 02:03:51 +0530

    test: refactor login to landing page tests

--- tests/landing.spec.ts
+++ tests/landing.spec.ts
@@ -1,31 +1,25 @@
 import test, { expect } from "@playwright/test";
 
-test("has logo icon", async ({ page }) => {
+test.beforeEach("go to landing page", async ({ page }) => {
   await page.goto("http://localhost:3000");
+})
 
+test("has logo icon", async ({ page }) => {
   await expect(page.getByAltText("ShareSplit logo")).toBeVisible();
 });
 
 test("has welcome message", async ({ page }) => {
-  await page.goto("http://localhost:3000");
-
   await expect(page.getByRole("heading", { name: "Welcome to"})).toBeVisible();
 })
 
 test("has logo text", async ({ page }) => {
-  await page.goto("http://localhost:3000");
-
   await expect(page.getByAltText("ShareSplit text")).toBeVisible();
 })
 
 test("has tag line", async ({ page }) => {
-  await page.goto("http://localhost:3000");
-
   await expect(page.getByRole("heading", { name: "Split your shares with your friends"})).toBeVisible();
 })
 
 test("hast login button", async ({ page }) => {
-  await page.goto("http://localhost:3000");
-
   await expect(page.getByRole("button", { name: "Login"})).toBeVisible();
 })
