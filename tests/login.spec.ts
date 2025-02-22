ff287d793b38c35e1d3ea00f197749880453598a
Rohit Rai <rohitraijob@gmail.com>
2025-02-23 01:44:33 +0530

    test: login page has welcome message

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -5,3 +5,9 @@ test("has logo icon", async ({ page }) => {
 
   await expect(page.getByAltText("ShareSplit logo")).toBeVisible();
 });
+
+test("has welcome message", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+
+  await expect(page.getByRole("heading", { name: "Welcome to"})).toBeVisible();
+})
