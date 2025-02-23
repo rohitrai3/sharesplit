4595e2d417df758a2c58bf867c0c6067094872c4
Rohit Rai <rohitraijob@gmail.com>
2025-02-23 12:45:57 +0530

    test: home page has logout button

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -2,12 +2,16 @@ import test, { expect } from "@playwright/test";
 
 test.beforeEach("go to home page", async ({ page }) => {
   await page.goto("http://localhost:3000/home");
-})
+});
 
 test("has username", async ({ page }) => {
-  expect(page.getByRole("link", { name: process.env.TEST_USERNAME}));
-})
+  expect(page.getByRole("link", { name: process.env.TEST_USERNAME }));
+});
 
 test("has create group button", async ({ page }) => {
   expect(page.getByRole("button", { name: "Create group" }));
-})
+});
+
+test("has logout button", async ({ page }) => {
+  expect(page.getByRole("button", { name: "Logout" }));
+});
