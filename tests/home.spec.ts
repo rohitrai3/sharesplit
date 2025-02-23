84eaece4070553ffda5f015dbc3a7b7114e12ee6
Rohit Rai <rohitraijob@gmail.com>
2025-02-23 16:02:31 +0530

    test: home page has loding while fetching group

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -19,3 +19,10 @@ test("has create group button", async ({ page }) => {
 test("has logout button", async ({ page }) => {
   await expect(page.getByRole("button", { name: "Logout" })).toBeVisible();
 });
+
+test("has loading while fetching groups", async ({ page }) => {
+  await expect(page.getByRole("heading", { name: "Loading..." })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: process.env.TEST_GROUP_UI })
+  ).toHaveCount(0);
+});
