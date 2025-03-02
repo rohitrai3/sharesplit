4db4536aefa8ac32a20596d4b7b4d07507c48f28
Rohit Rai <rohitraijob@gmail.com>
2025-03-02 12:42:43 +0530

    test: add member page has username after loading is finished

--- tests/addMember.spec.ts
+++ tests/addMember.spec.ts
@@ -1,6 +1,6 @@
 import test, { expect } from "@playwright/test";
 
-test.beforeEach("go to view add member page", async ({ page }) => {
+test.beforeEach("go to add member page", async ({ page }) => {
   await page.goto(process.env.TEST_ADD_MEMBER_PAGE_URL!);
 });
 
@@ -11,4 +11,13 @@ test("has loading while fetching user", async ({ page }) => {
   await expect(
     page.getByRole("link", { name: process.env.TEST_USERNAME })
   ).toHaveCount(0);
+});
+
+test("has username after loading is finished", async ({ page }) => {
+  await expect(
+    page.getByRole("heading", { name: "Loading user..." })
+  ).toBeHidden();
+  await expect(
+    page.getByRole("link", { name: process.env.TEST_USERNAME })
+  ).toBeVisible();
 });
