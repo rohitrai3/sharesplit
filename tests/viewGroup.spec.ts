ba9b0b6c705ae2f1f38451f9069b4fa10ea3b954
Rohit Rai <rohitraijob@gmail.com>
2025-03-02 11:31:39 +0530

    test: view group page has loading while fetching user

---
+++ tests/viewGroup.spec.ts
@@ -0,0 +1,14 @@
+import test, { expect } from "@playwright/test";
+
+test.beforeEach("go to view group page", async ({ page }) => {
+  await page.goto(process.env.TEST_VIEW_GROUP_PAGE_URL!);
+});
+
+test("has loading while fetching user", async ({ page }) => {
+  await expect(
+    page.getByRole("heading", { name: "Loading user..." })
+  ).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: process.env.TEST_USERNAME })
+  ).toHaveCount(0);
+});
